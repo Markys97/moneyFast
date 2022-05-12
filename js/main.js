97c8a5c466d517c02afd5846efd0ef7a70f43317
Markys97 <70927348+Markys97@users.noreply.github.com>
2022-05-12 18:01:55 +0300

    baisse

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ const getDollars=()=>{
 }
 const getRouble=()=>{
     let montantTransfertValue=  parseFloat(montantTransfert.value);
-    let rouble= montantTransfertValue * 0.10;
+    let rouble= montantTransfertValue * 0.9;
 
     return rouble.toFixed(2)
 }
