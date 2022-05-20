cc5c5afd050f8dae53cfa57c29ac4576a658c7b3
Markys97 <70927348+Markys97@users.noreply.github.com>
2022-05-20 15:05:59 +0300

    mma

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ const getDollars=()=>{
 }
 const getRouble=()=>{
     let montantTransfertValue=  parseFloat(montantTransfert.value);
-    let rouble= montantTransfertValue*0.8/100 ;
+    let rouble= montantTransfertValue*0.8/10 ;
 
     return rouble.toFixed(2)
 }
